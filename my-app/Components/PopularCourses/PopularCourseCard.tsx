47d24df923ffe7a9e04e4148056c4fb107babe31
Mikel <mickis2010.mj@gmail.com>
2026-05-11 16:18:03 +0200

    Popular Courses map created with components

---
+++ my-app/Components/PopularCourses/PopularCourseCard.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import PopularCourseText from "./PopularCourseText";
+
+type Props = {
+    Image: string,
+    PopularTitle: string,
+    PopularDescription: string,
+    Button: string,
+}
+
+const PopularCard = ({Image, PopularTitle, PopularDescription, Button}: Props) => {
+  return (
+    
+  )
+}
---
+++ my-app/Components/PopularCourses/PopularCourseText.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {
+    PopularTitle: string,
+    PopularDescription: string,
+}
+
+const PopularText = ({PopularTitle, PopularDescription}: Props) => {
+  return (
+    <div>PopularText</div>
+  )
+}
